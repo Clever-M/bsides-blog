43e2e7c2d79d1cf041243f570f7834d992ed2ad9
Clever Ronni <ronniclever@gmail.com>
2023-11-23 00:34:43 -0300

    Creates a post model

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,11 @@
+class BlogPostsController < ApplicationController
+  def index
+    @posts = BlogPost.all
+  end
+
+  def show
+    @post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+end
